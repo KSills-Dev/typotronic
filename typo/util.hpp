e486222e58912a507232eee0e68a7e3320d49857
Kenneth Sills <KSills.Dev@Gmail.com>
2017-07-15 15:45:40 -0400

    Documentation added to util.hpp

--- typo/util.hpp
+++ typo/util.hpp
@@ -2,5 +2,12 @@
 
 #include <vector>
 
+/**
+ @brief Returns the minimum value from a vector of elements.
+ */
 auto min(std::vector<int> values) -> int;
+
+/**
+ @brief Returns the maximum value from a vector of elements.
+ */
 auto max(std::vector<int> values) -> int;
