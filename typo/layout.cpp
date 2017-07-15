b238774b7a8da15001ada587e89a88a8d3561146
Kenneth Sills <KSills.Dev@Gmail.com>
2017-07-15 14:43:57 -0400

    Documentation and commenting for layout files.

--- typo/layout.cpp
+++ typo/layout.cpp
@@ -1,34 +1,31 @@
 #include "layout.hpp"
 #include <cstddef>
 
-/**
- @brief
- */
 auto find_char_in_table(const char c) -> Coord {
   for (size_t i = 0; i < key_table.size(); i++) {
     for (size_t j = 0; j < key_table[i].size(); j++) {
       if (key_table[i][j] == c)
+        // Key found, return coordinate.
         return Coord{static_cast<int>(i), static_cast<int>(j)};
     }
   }
   return Coord();
 }
 
-/**
- @brief
- */
 auto hand_for_char(const Coord place) -> Hand {
-  return (place.col / 5 == 1) ? Hand::Left : Hand::Right;
+  // Left hand col < 5, right hand col >= 5.
+  return static_cast<Hand>(place.col / 5);
 }
 
-/**
- @brief
- */
 auto finger_for_char(const Coord place) -> Finger {
   auto col = place.col;
+
   if (col / 5 == 1) {
+    // Invert order and reduce value for right hand.
     col = col * -1 + 9;
   }
+
+  // Pointer finger used for last 2 columns each hand.
   col = (col == 4) ? 3 : col;
   return static_cast<Finger>(col);
 }
--- typo/layout.hpp
+++ typo/layout.hpp
@@ -2,10 +2,17 @@
 
 #include "util.hpp"
 
+/**
+ @brief Sentinel value appended to start/end of strings during typo check.
+
+ @note Must not be \0 - std::string::length will report inaccurately.
+ */
 static const char sentinel = '\t';
 
 /**
- @brief
+ @brief POD describing keyboard layout coordinates for a given character.
+
+ @see find_char_in_table
  */
 struct Coord {
   int row;
@@ -13,17 +20,20 @@ struct Coord {
 };
 
 /**
- @brief
+ @brief Hand used to press a given key.
  */
-enum class Hand { Left, Right };
+enum class Hand { Left = 0, Right = 1 };
 
 /**
- @brief
+ @brief Finger used to press a given key - hand independent.
  */
 enum class Finger { Pinky = 0, Ring = 1, Middle = 2, Pointer = 3 };
 
 /**
- @brief
+ @brief Keyboard layout for coordinate finding.
+
+ @see Coord
+ @see find_char_in_table
  */
 static const std::vector<std::vector<char>> key_table{
     {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'},
@@ -32,18 +42,35 @@ static const std::vector<std::vector<char>> key_table{
     {'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.'}};
 
 /**
- @brief
+ @brief Returns the coordinates for the given character c in the keyboard layout
+ table.
+
+ @see Coord
  */
 auto find_char_in_table(const char c) -> Coord;
 
 /**
- @brief
+ @brief Returns the hand used to strike a key at the given place.
+
+ @see Hand
+ @see find_char_in_table
  */
 auto hand_for_char(const Coord place) -> Hand;
 
 /**
- @brief
+ @brief Returns the finger used to strike a key at the given place, independent
+ of hand.
+
+ @see Finger
+ @see find_char_in_table
  */
 auto finger_for_char(const Coord place) -> Finger;
 
+/**
+ @brief Returns the distance between the lhs and rhs keys.
+
+Formula is `dist = max(|row_1 - row_2|, |col_1 - col_2|)`.
+
+ @see find_char_in_table
+ */
 auto distance(const Coord lhs, const Coord rhs) -> int;
